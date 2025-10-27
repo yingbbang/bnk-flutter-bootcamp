687db7db9d117935f93e837f7a74fb8ccab18c32
yingbbang <yingbbang@naver.com>
2025-10-27 10:51:35 +0900

    feat(ch03): add GridView widget example and update ListView widget

--- ch03/lib/01_listview_widget.dart
+++ ch03/lib/01_listview_widget.dart
@@ -12,7 +12,10 @@ void main(){
   //runApp(ListViewTest1());
 
   // ListView.builder 실습
-  runApp(ListViewTest2());
+  //runApp(ListViewTest2());
+
+  // ListView.seperated 실습
+  runApp(ListViewTest3());
 }
 
 class ListViewTest1 extends StatelessWidget {
@@ -26,7 +29,7 @@ class ListViewTest1 extends StatelessWidget {
         appBar: AppBar(title: Text('01.ListView 위젯 실습'),),
         body: ListView(
           children: [
-            for(int i=1 ; i<=10 ; i++)
+            for(int i=1 ; i<=5 ; i++)
               Container(
                 padding: EdgeInsets.all(10),
                 margin: EdgeInsets.all(10),
@@ -40,6 +43,27 @@ class ListViewTest1 extends StatelessWidget {
                 ),
                 child: Text('${i}번째 항목'),
               ),
+
+            // ListTile은 리스트 아이템 항목을 구성하는 위젯
+            ListTile(
+              leading: const Icon(Icons.sunny),
+              title: const Text('ListTile 제목'),
+              subtitle: const Text('ListTile 내용'),
+              trailing: const Icon(Icons.menu),
+            ),
+
+            ListTile(
+              leading: CircleAvatar(
+                radius: 20,
+                backgroundImage: NetworkImage('https://picsum.photos/id/237/200/300'),
+              ),
+              title: const Text('ListTile 제목'),
+              subtitle: const Text('ListTile 내용'),
+              trailing: const Icon(Icons.more_vert),
+              onTap: (){
+                print('클릭!');
+              },
+            ),
           ],
         ),
       ),
@@ -81,3 +105,66 @@ class ListViewTest2 extends StatelessWidget {
     );
   }
 }
+
+class ListViewTest3 extends StatelessWidget {
+  const ListViewTest3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    List<User> userList = [
+      User('a101', '김유신', '1990-09-01', 23, 'https://picsum.photos/id/237/200/300'),
+      User('a102', '김춘추', '1991-09-02', 21, 'https://picsum.photos/id/238/200/300'),
+      User('a103', '장보고', '1992-09-03', 33, 'https://picsum.photos/id/239/200/300'),
+      User('a104', '강감찬', '1993-09-04', 43, 'https://picsum.photos/id/240/200/300'),
+      User('a105', '이순신', '1994-09-05', 53, 'https://picsum.photos/id/241/200/300'),
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('03.ListView.seperated 실습'),),
+        body: ListView.separated(           // ListView.separated는 ListView.builder에서 목록 구분선을 커스텀 할 수 있는 위젯
+          itemCount: userList.length,       // 목록에 출력할 총 아이템 갯수
+          itemBuilder: (context, index){    // 인덱스를 기반으로 각 아이템 위젯 생성
+            return ListTile(
+              leading: CircleAvatar(
+                radius: 20,
+                backgroundImage: NetworkImage(userList[index].avatar),
+              ),
+              title: Text('${userList[index].name}(${userList[index].userid})'),
+              subtitle: Text('${userList[index].age}세, ${userList[index].birth}'),
+              trailing: const Icon(Icons.more_vert),
+              onTap: (){
+                print('클릭!');
+              },
+            );
+          },
+          separatorBuilder: (BuildContext context, int index){
+            return Divider(height: 1, color: Colors.black,);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+// User 모델(Spring의 DTO역할)
+class User {
+  String userid;
+  String name;
+  String birth;
+  int age;
+  String avatar;
+
+  User(this.userid, this.name, this.birth, this.age, this.avatar);
+}
+
+
+
+
+
+
+
+
+
+
---
+++ ch03/lib/02_gridview_widget.dart
@@ -0,0 +1,51 @@
+/*
+  날짜 : 2025/10/27
+  이름 : 조지영
+  내용 : GridView 위젯 실습하기
+*/
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(GridViewTest());
+
+}
+
+class GridViewTest extends StatelessWidget {
+  //const GridViewTest({super.key});
+
+  List<String> cities = ['서울', '대전', '대구', '부산', '광주'];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('02.GridView 위젯 실습'),
+          ),
+          body: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,          // 한줄에 표시할 아이템 갯수
+                  crossAxisSpacing: 10,       // 교차축(가로) 그리드 아이템 간격
+                  mainAxisSpacing: 10,        // 메인축(세로) 그리드 아이템 간격
+                  childAspectRatio: 1 / 1.5   // 그리드 아이템 가로/세로 비율 조정
+              ), // grid 형태 정의
+              itemCount: cities.length,
+              itemBuilder: (context, index){
+
+                return Card(
+                  child: Center(
+                    child: Text(cities[index]),
+                  ),
+                );
+
+              }
+          )
+      ),
+    );
+  }
+
+}
